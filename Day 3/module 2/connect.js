6ebd087a74a02c4fdce1eb63d687bf66ea93bcb7
Nafisur Ahmed <cse.nafisur@gmail.com>
2019-11-22 11:27:22 +0530

    day 3

---
+++ Day 3/module 2/connect.js
@@ -0,0 +1,37 @@
+var mysql = require('mysql');
+const express = require('express')
+var app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'my_db'
+});
+ 
+connection.connect((err)=> {
+    if (!err) {
+        console.log("DB connection successfull")
+    }
+    else {
+        console.log(JSON.stringify(err, undefined,2))
+    }
+})
+
+app.get('/employees', (req, res)=>{
+
+    var sql_query = 'select * from employee'
+    connection.query(sql_query,(err,rows, fields)=>{
+        if (!err) {
+            res.send(rows)
+            connection.end()
+        }
+        else{
+            console.log(err)
+        }
+    })
+})
+app.listen(3000, ()=> console.log('App running'))
